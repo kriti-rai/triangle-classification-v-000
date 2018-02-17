04ebfa6e27b097f27cda05f07ffdbc82a96fa05c
Kriti Rai <kriti.rai@gmail.com>
2018-02-17 21:32:49 +0000

    automatically backed up by learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -9,7 +9,7 @@ class Triangle
   end
 
   def valid?
-    if (@length_1 > 0) && (@length_2 > 0) && (@length_3 > 0)
+    if (@length_1 > 0) && (@length_2 > 0) && (@length_3 > 0) && ((@length_1 + @length_2) > @length_3) && ((@length_1 + @length_3) > @length_2) && ((@length_2 + @length_3) > @length_1)
       true
     else
       false
