105b6627d2b78c3ef6b3409c2b469c3b82833a74
Kriti Rai <kriti.rai@gmail.com>
2018-02-17 21:24:37 +0000

    automatically backed up by learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,44 @@
+require 'pry'
 class Triangle
-  # write code here
+  attr_accessor :length_1, :length_2, :length_3
+
+  def initialize(length_1, length_2, length_3)
+    @length_1 = length_1
+    @length_2 = length_2
+    @length_3 = length_3
+  end
+
+  def valid?
+    if (@length_1 > 0 && @length_2 > 0 && @length_3 > 0) && ((@length_1 + @length_2) > @length_3) && ((@length_1 + @length_3) > @length_2) && ((@length_2 + @length_3) > @length_1)
+      true
+    else
+      false
+    end
+
+  end
+    # binding.pry
+  def kind
+    if !valid?
+      begin
+        raise TriangleError.new
+      rescue TriangleError => error
+        puts error.message
+      end
+
+    elsif @length_1 == @length_2 && @length_2 == @length_3
+      :equilateral
+    elsif ((@length_1==@length_2)&& @length_1!= @length_3)||((@length_3 == @length_1) && @length_3 != @length_2)||((@length_2 == @length_3) && @length_2 != @length_1)
+      :isosceles
+    elsif @length_1!= @length_2 && @length_1!= @length_3 && @length_2!= @length_3
+      :scalene
+    end
+  end
+end
+
+class TriangleError < StandardError
+  def initialize
+  end
+  def message
+    "It is not a valid triangle."
+  end
 end
